414ccfcf1ffaf57b7e748ce5479d62c111c6a058
Charlie Arriaga <arriayaa@gmail.com>
2023-12-04 17:46:03 -0800

    started error message

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import axios from 'axios'
+import ErrorModal from 'src/components/ErrorModal'
 import './App.css'
 
 const API_KEY = import.meta.env.VITE_API_KEY;
@@ -8,17 +9,23 @@ function App() {
 
   const [location, setLocation] = useState({ display_name: 'City' }, { lat: '47.6038321' }, { lon: '-122.330062' });
   const [searchQuery, setSearchQuery] = useState('');
+  const [modalState, setModalState] = useState(false)
 
   async function fetchLocation() {
     const API = `https://us1.locationiq.com/v1/search.php?key=${API_KEY}&q=${searchQuery}&format=json`;
     const response = await axios.get(API);
     const locationObj = response.data[0];
     setLocation(locationObj);
-    console.log(locationObj)
+
   }
 
   function updateQuery(event) {
     setSearchQuery(event.target.value)
+  }
+
+  function errorMessage (message) {
+    setModalState(true)
+    return message;
   }
 
   return (
@@ -27,6 +34,7 @@ function App() {
         <input onChange={updateQuery} />
         <button onClick={fetchLocation}>Explore!</button>
       </div>
+      <ErrorModal show={modalState} message={errorMessage}/>
       <h1>{location.display_name} Information</h1>
       <h2>{location.lat} latitude, {location.lon} longitute</h2>
       <img src={`https://maps.locationiq.com/v3/staticmap?key=${API_KEY}&center=${location.lat},${location.lon}&zoom=12&size=900x400&format=jpg&maptype=light/`} />
