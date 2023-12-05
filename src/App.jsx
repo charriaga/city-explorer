f93989e24aa13b6b34c84c513f7eaa0e5d87dcaf
Charlie Arriaga <arriayaa@gmail.com>
2023-12-04 18:10:43 -0800

    made error messages

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,48 @@
 import { useState } from 'react'
 import axios from 'axios'
-import ErrorModal from 'src/components/ErrorModal'
 import './App.css'
 
 const API_KEY = import.meta.env.VITE_API_KEY;
 
 function App() {
 
-  const [location, setLocation] = useState({ display_name: 'City' }, { lat: '47.6038321' }, { lon: '-122.330062' });
+  const [location, setLocation] = useState({ display_name: 'City' });
   const [searchQuery, setSearchQuery] = useState('');
-  const [modalState, setModalState] = useState(false)
 
+  //ChatGPT was consulted for this function
   async function fetchLocation() {
     const API = `https://us1.locationiq.com/v1/search.php?key=${API_KEY}&q=${searchQuery}&format=json`;
-    const response = await axios.get(API);
-    const locationObj = response.data[0];
-    setLocation(locationObj);
-
+    try {
+      const response = await axios.get(API);
+      const locationObj = response.data[0];
+      setLocation(locationObj);
+      if (!response.ok) { if (response.status === 401) {
+          h1Message('error');
+      } else {
+        console.error('Error: status of ', response.status);
+        return;
+      } }
+    } catch (error) {
+      console.error('Error: undefined');
+    }
   }
 
   function updateQuery(event) {
     setSearchQuery(event.target.value)
   }
 
-  function errorMessage (message) {
-    setModalState(true)
-    return message;
+  function h1Message (parameter) {
+    if (parameter === 'error') {
+      return 'API INVALID';
+    } else {
+      return (location.display_name + ' Information')
+    }
   }
+
+  // function errorMessage(message) {
+  //   setModalState(true)
+  //   return message;
+  // }
 
   return (
     <>
@@ -34,8 +50,7 @@ function App() {
         <input onChange={updateQuery} />
         <button onClick={fetchLocation}>Explore!</button>
       </div>
-      <ErrorModal show={modalState} message={errorMessage}/>
-      <h1>{location.display_name} Information</h1>
+      <h1>{h1Message('fine')}</h1>
       <h2>{location.lat} latitude, {location.lon} longitute</h2>
       <img src={`https://maps.locationiq.com/v3/staticmap?key=${API_KEY}&center=${location.lat},${location.lon}&zoom=12&size=900x400&format=jpg&maptype=light/`} />
     </>
